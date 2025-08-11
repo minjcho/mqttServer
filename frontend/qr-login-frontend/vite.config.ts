7810fdf6ba5325a89234f22567127aa54c24df9f
minjcho <131win1@gmail.com>
2025-08-11 16:53:38 +0900

    [add] tls 인증서 설정

--- frontend/qr-login-frontend/vite.config.ts
+++ frontend/qr-login-frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,6 +8,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    https: {
+      key: fs.readFileSync('./certs/key.pem'),
+      cert: fs.readFileSync('./certs/cert.pem')
+    },
     proxy: {
       '/api': {
         target: 'http://3.36.126.83:8090',
